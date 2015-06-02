9837c162d6f40b087ecc922cd25faa51b60220f1
apple <apple@appletekiMacBook-Pro.local>
2015-06-02 10:37:20 +0800

    Initial Commit

---
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+import java.lang.System;
+
+
+public class  BibliotecaApp{
+    public static void main(String[] args){
+        System.out.print("Helloworld");
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/ExampleTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by apple on 15-6-2.
+ */
+public class ExampleTest {
+    @Test
+    public void testName() throws Exception {
+        assertEquals(1,1);
+
+    }
+}
